3fbb2c2cf56095ab18f28b2892cdf50e3e3569d1
Rave3 <babyrave333@gmail.com>
2022-12-02 12:38:05 +0100

    day 02 - 02

--- 02/main.py
+++ 02/main.py
@@ -1,5 +1,5 @@
 __author__ = "Chiara PR"
-__version__ = "01.01"
+__version__ = "01.02"
 __date__ = "2022-12-02"
 
 # LOSS = 0
@@ -7,38 +7,47 @@ DRAW = 3
 WIN = 6
 SCORE = 0
 
-# opponent = {"A" : "Rock", "B" : "Paper", "C" : "Scissors"}
-# player = {"X" : 1, "Y" : 2, "Z" : 3}
+ROCK = 1
+PAPER = 2
+SCISSORS = 3
+
+opponent = {"Rock" : "A","Paper" : "B", "Scissors" :  "C"}
+# player = {"X" : "loss", "Y" : "draw", "Z" : "win"}
 
 f_in = "./input.txt"
 
 with open(f_in, 'r', encoding='utf-8') as f:
     lines = f.readlines()
-    print(lines)
 
     for line in lines:
         # print(SCORE)
         o = line.split()[0]
-        p = line.split()[1]
+        end = line.split()[1]
 
-        if p == 'X':
-            SCORE += 1
-            if o == 'A':
-                SCORE += DRAW
-            elif o == 'C':
-                SCORE += WIN
-        elif p == 'Y':
-            SCORE += 2
-            if o == 'B':
-                SCORE += DRAW
-            elif o == 'A':
-                SCORE += WIN
-        elif p == 'Z':
-            SCORE += 3
-            if o == 'C':
-                SCORE += DRAW
-            elif o == 'B':
-                SCORE += WIN
-
+        # use 3.10 to optimize using switch/case
+        if end == 'X': # LOSS
+            # SCORE += LOSS (0)
+            if o == opponent['Rock']:
+                SCORE += SCISSORS
+            elif o == opponent['Paper']:
+                SCORE += ROCK
+            else: # o == 'Scissors':
+                SCORE += PAPER
+        elif end == 'Y': # DRAW
+            SCORE += DRAW
+            if o == opponent['Rock']:
+                SCORE += ROCK
+            elif o == opponent['Paper']:
+                SCORE += PAPER
+            else: # o == 'Scissors':
+                SCORE += SCISSORS
+        elif end == 'Z': # WIN
+            SCORE += WIN
+            if o == opponent['Rock']:
+                SCORE += PAPER
+            elif o == opponent['Paper']:
+                SCORE += SCISSORS
+            else: # o == 'Scissors':
+                SCORE += ROCK
 
 print(SCORE)
